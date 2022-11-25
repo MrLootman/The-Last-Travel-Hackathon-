273cc5be423de1ad81c0b2e497496d95967a3088
NaoSan9 <naomi.santos@outlook.fr>
2022-11-25 13:04:34 +0100

    Agency page and cards functional

--- src/pages/Agency.jsx
+++ src/pages/Agency.jsx
@@ -2,26 +2,39 @@ import React from 'react';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer';
 import { characters } from '../data.js';
+import '../style/Agency.css'
 
 const Agency = () => {
     return (
         <div>
             <Header />
-            <div>
-                {/* {characters.map((character) => {
+            <div className='cardContainer'>
+                {characters.map((character) => {
                     return (
-                        <div className='cardContainer' key={character.id}>
+                        <div key={character.id} image={character.image} name={character.name} age={character.age}
+                            sign={character.sign}
+                            beliefs={character.beliefs}
+                            degree={character.degree}
+                            speciality={character.speciality}>
+                            <div className='card'>
+                                <div className='photoPortrait'><img src={character.image} alt="Agent Portrait" /></div>
+                                <h3>{character.name}</h3>
+                                <div className='cardContent'>
+                                    <p><span>Âge : </span>{character.age} ans</p>
+                                    <p><span>Signe particulier : </span>{character.sign}</p>
+                                    <p><span>Diplôme : </span>{character.degree}</p>
+                                    <p><span>Spécialité : </span>{character.speciality}</p>
+                                </div>
+                            </div>
 
-                            <img src={character.image} alt="Agent Portrait" />
-                            <h3>{characters}</h3>
 
                         </div>
                     )
-                })} */}
+                })}
             </div>
-
             <Footer />
-        </div>
+
+        </div >
     );
 };
 
